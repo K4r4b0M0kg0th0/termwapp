9590c5910fd4eafd23356fd30e40e74af25fb2b0
b4p3d1 <mokgotho66@outlook.com>
2023-01-14 22:50:44 +0200

    changed firefox webdriver to chrome webdriver

--- termwapp.py
+++ termwapp.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
+
 class Termwapp:
-    def __init__(self):
-        self.driver = webdriver.Firefox()
+    def __init__(self, driver):
+        self.driver = driver
         self.driver.get("https://web.whatsapp.com/")
         input("Scan the QR code and press enter to continue...")
+        
+    def close_driver(self):
+        self.driver.close()
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    tp = Termwapp(driver)
+    #other functionalities
+    tp.close_driver()
